6123068b546a2672be26831d1a5a9adb62043dc2
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-30 13:26:42 -0400

    setup axios and request photos from Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID oyr14NJUtqgGk-2jaLR7jLbI8bgD08q1QpfQG6f2QxQ",
+      },
+    });
   }
   render() {
     return (
